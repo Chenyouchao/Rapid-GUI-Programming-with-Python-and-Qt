fdebe2356f19e4161675c07dfbc0343509a2068e
chenyouchao <845462651@qq.com>
2020-03-07 10:13:11 +0800

    添加笔记

--- Chapter-04/alert.py
+++ Chapter-04/alert.py
@@ -1,3 +1,12 @@
+'''
+    P83 4.1 25行的弹出式闹钟
+    1. 在当前目录输入一下指令运行：
+        "python alert.py 10:00 WakeUp"
+        (书上的指令不含python，无法运行)
+    2. sys.argv的参数从0开始计数，sys.argv[0]代表"python"
+    3. pyqt5的模块做了调整，按照书上的模块导入会发生错误
+'''
+
 import sys
 import time
 from PyQt5.QtGui import *
